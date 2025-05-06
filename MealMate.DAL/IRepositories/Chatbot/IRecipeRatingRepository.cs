9908e487531264b9d3130681dbd25b2e9bf6bf3d
Phuc000 <truongvinhphuc2003@gmail.com>
2025-05-06 22:15:01 +0700

    feat: add some recipe api

--- MealMate.DAL/IRepositories/Chatbot/IRecipeRatingRepository.cs
+++ MealMate.DAL/IRepositories/Chatbot/IRecipeRatingRepository.cs
@@ -4,6 +4,8 @@ namespace MealMate.DAL.IRepositories.Chatbot
 {
     public interface IRecipeRatingRepository
     {
+        Task<List<RecipeRating>> GetAllAsync();
+        Task<List<RecipeRating>> GetRecipeRatingByCustomerIdAsync(Guid customerId);
         Task<RecipeRating?> GetRecipeRatingAsync(Guid recipeId, Guid customerId);
         Task<List<RecipeRating>> GetAllRatingByRecipeIdAsync(Guid recipeId);
         Task CreateAsync(RecipeRating entity);
